f88e55d52e67fb5588dca082ef4f3a91df541cf1
adityagrace <aditya.grace@gmail.com>
2025-11-11 21:41:57 +0700

    fix customers id route

--- app/api/customers/[id]/route.ts
+++ app/api/customers/[id]/route.ts
@@ -1,14 +1,19 @@
 // app/api/customers/[id]/route.ts
 
 import { NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
+import { prisma } from "@/prisma";
 
-const prisma = new PrismaClient();
+
+
+//import { PrismaClient } from '@prisma/client';
+
+//const prisma = new PrismaClient();
 
 // Fungsi DELETE (sudah ada)
 export async function DELETE(
   request: Request,
-  { params }: { params: Promise<{ id: string }> }
+  { params }: { params: { id: string } }
+
 ) {
   try {
     const { id } = await params;
